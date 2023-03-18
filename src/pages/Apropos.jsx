568dd35a6feafb675281a5dfdaa5af621fe8cd90
laouinis <laouinis@gmail.com>
2023-03-18 05:14:49 +0100

    file and style changed and fixed

--- src/pages/Apropos.jsx
+++ src/pages/Apropos.jsx
@@ -1,16 +1,16 @@
 /* eslint-disable max-len */
 /* eslint-disable require-jsdoc */
 import React from 'react';
-import Collapse from './Collapse';
+import Collapse from '../components/Collapse';
 import CollapseData from '../data/collapse.json';
-// import Banner from './Banner';
+import Banner from '../components/Banner';
+import banner2 from '../assets/banner2.png';
 import '../styles/App.css';
 
 function Apropos() {
   return (
     <React.Fragment>
-      {/* <Banner source='../assets/banner2.png' showText={false} /> */}
-      <div className='banner'></div>
+      <Banner image={banner2} showText={false}/>
       <main className='accordion'>
         {CollapseData.map((item, index) => (
           <Collapse key={index} {...item} />
