6e55588f80bdc119466ebfff9f8aeeea52fc1e83
laouinis <laouinis@gmail.com>
2023-03-18 07:19:15 +0100

    rates.jsx created

---
+++ src/components/Rates.jsx
@@ -0,0 +1,37 @@
+/* eslint-disable require-jsdoc */
+
+import React from 'react';
+import PropTypes from 'prop-types';
+import Star from '../assets/Star.png';
+import StarEmpty from '../assets/StarEmpty.png';
+function Rate({logement}) {
+  const ratingScore = [1, 2, 3, 4, 5];
+  return (
+    <div>
+      {ratingScore.map((ratingElem) =>
+
+        logement >= ratingElem ? (
+          <img
+            src={Star}
+            key={ratingElem.toString()}
+            alt='filled star'
+          />
+        ) : (
+          <img
+            src={StarEmpty}
+            key={ratingElem.toString()}
+            alt='empty star'
+          />
+        ),
+      )}
+    </div>
+  );
+}
+
+Rate.propTypes = {
+  logement: PropTypes.number.isRequired,
+};
+
+export default Rate;
+
+
